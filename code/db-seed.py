0c7d11d8bfb647ac60fd0e78bbe999dad16416ce
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2024-03-29 01:44:39 +0100

    added db-seed.py file

--- code/db-seed.py
+++ code/db-seed.py
@@ -1,4 +1,9 @@
+# Import Flask modules
+from flask import Flask, request, render_template
 from flaskext.mysql import MySQL
+
+# Create an object named app
+app = Flask(__name__)
 
 # This "/home/ec2-user/dbserver.endpoint" file has to be created from cloudformation
 # template and it has RDS endpoint
